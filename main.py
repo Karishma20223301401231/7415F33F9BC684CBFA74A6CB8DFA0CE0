a90f411c38e00bf7ed9dc16625a06acc98ea83c0
Karishma20223301401231 <144519575+Karishma20223301401231@users.noreply.github.com>
2023-09-17 20:05:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,58 @@
-def fact_rec(n):
-  if (n == 0) or (n == 1):
-    return 1
-  else:
-    return n * fact_rec(n - 1)
+# BankAccount class 
+class Bankaccount:
 
+  def __init__(self):
 
-num = 6
-result = fact_rec(num)
-print("The factorial of 3 is", (num, result))
+# Function to deposit amount
+   def deposit(self):
+      amount =float(input("enter                amount to be deposited:"))       
+      self.balance+=amount
+      print("\n Amount                              Deposited:",amount)
+
+# Function to withdraw the amount
+def withdraw (self):
+     amount=float(input("enter amount to be withdrawn:"))
+     if self.balance>=amount:
+       self.balance-=amount
+       print("\n you withdraw:, amount")
+     else:
+       print("\n insufficient balance ")
+
+# Function to display the amount
+def display(self):
+        print("\n New available balance=", self.balance)
+
+# python program to create Bank account class 
+# with both a deposit() and a withdraw () function
+class  Bank_Account:
+    def __init__(self):
+      self.balance=0
+      print("Hello!!! Welcome to the deposit & withdrawal machine ")
+
+    def deposit(self):
+        amount=float(input("enter amount to be deposited"))
+        self.balance+=amount
+        print ("\n amount deposited:", amount)
+
+    def withdraw(self):
+        amount=float(input("enter amount to be withdraw:"))
+        if self.balance>=amount:
+           self.balance-=amount
+           print("\n you withdraw:", amount)
+        else:
+          print("\n insufficient balance ")
+
+    def display(self):
+        print("\n new available balance=",self.balance)
+
+# Driver code
+
+#creating an object of class
+s=Bank_Account()
+
+# calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
+    
+     
