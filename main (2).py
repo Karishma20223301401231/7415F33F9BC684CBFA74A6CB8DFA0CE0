5c812306055817000446465250ee0ae505f415ff
Karishma20223301401231 <144519575+Karishma20223301401231@users.noreply.github.com>
2023-09-25 18:50:16 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,10 +1,12 @@
-def isleapyear(year):
-  if(year/4==0 and year/100!=0) or year/ 400==0:
-    return True
-  else:
-    return False
-year=2020
-if isleapyear(year):
-  print("2020 is a leap year ")
-else:
-  print("2020 is a not a leap year")
+def linearSearchProduct(productList,targetProduct):
+  indices=[]
+  for index,product in enumerate(productList):
+    if product==targetProduct:
+      indices.append(index)
+  return indices
+#example usage
+products=["shoes","boots","loafer","shoes","sandals","shoes"]
+target1="shoes"
+target2="apples"
+result=linearSearchProduct(products,target1)
+print(result)
